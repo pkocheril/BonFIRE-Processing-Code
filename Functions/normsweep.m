3ae75f02790f8b5192cdb6fe692c4dd66a3e8b91
Philip Kocheril <pkocheril@gmail.com>
2023-05-01 16:36:00 -0700

    """Initial baseline correction (baselinecorrect_temporalv1)"""

--- Functions/normsweep.m
+++
@@ -1,7 +0,0 @@
-% Normalize a signal
-function NORMSIG = normsweep(SIGNAL)
-%%% This function background-subtracts and normalizes a vector.
-    
-    NORMSIG = (SIGNAL-min(SIGNAL))./(max(SIGNAL)-min(SIGNAL));
-    return
-end
